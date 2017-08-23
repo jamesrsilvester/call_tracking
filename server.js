5eda5e5ce0a8099da9342474c8c36d001bfc66b6
jamesrsilvester <jamesrsilvester@gmail.com>
2017-08-23 09:48:32 -0700

    reverted to public

--- server.js
+++ server.js
@@ -6,17 +6,11 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-// // At the top of your web.js
-// process.env.PWD = process.cwd()
-//
-// // Then
-// app.use(express.static(process.env.PWD + 'public'));
-
 app.post('/voice', (request, response) => {
   const twiml = new VoiceResponse();
   // twiml.say('hello. Say something different next time
   twiml.say({ voice: 'alice' }, 'Hello there. This message is awaiting customization. And now, some Cowbell.');
-  twiml.play('https://agile-sands-23686.herokuapp.com/media/boom2.mp3');
+  twiml.play('https://agile-sands-23686.herokuapp.com/boom2.mp3');
   response.type('text/xml');
   response.send(twiml.toString());
 });
